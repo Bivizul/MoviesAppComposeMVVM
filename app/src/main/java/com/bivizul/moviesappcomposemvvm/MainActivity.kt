d62f64634555a38e70f4fd37133bcbbd3274208e
Bivizul <mamochapa@gmail.com>
2022-04-19 15:11:55 +0200

    Initial commit

---
+++ app/src/main/java/com/bivizul/moviesappcomposemvvm/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.bivizul.moviesappcomposemvvm
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.bivizul.moviesappcomposemvvm.ui.theme.MoviesAppComposeMVVMTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MoviesAppComposeMVVMTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MoviesAppComposeMVVMTheme {
+        Greeting("Android")
+    }
+}
